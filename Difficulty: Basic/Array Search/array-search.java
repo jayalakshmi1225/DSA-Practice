8444bcee3ba220d16a69e1981a08531275a451bb
Jayalakshmi Lakkavajjala <118071175+jayalakshmi1225@users.noreply.github.com>
2025-06-17 23:08:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.java
@@ -0,0 +1,17 @@
+
+
+class Solution {
+
+    static int search(int arr[], int x) {
+
+        // Your code here
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+          
+        }
+}
